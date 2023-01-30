faac2b1bfc9ddf30ac43357b77db9ae00e6201c8
BSLang2006 <65793131+BSLang2006@users.noreply.github.com>
2023-01-30 09:53:29 -0500

    Add files via upload
    Our first file to collaborate on

---
+++ BarManager.java
@@ -0,0 +1,18 @@
+// Your First Program
+
+class BarManager{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bar Inventory Manager"); 
+    
+    int volume = 1;
+
+    System.out.printf("Volume of the first bottle is: %d", volume);
+    
+    
+    
+    
+    }
+
+
+
+}
