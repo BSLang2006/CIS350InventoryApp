de256e5d7e372a798756c768ea90005b2413abd1
brandon <brandonscottlang@gmail.com>
2023-01-31 10:52:32 -0500

    added a liquor class

--- BarManager.java
+++ BarManager.java
@@ -7,7 +7,12 @@ class BarManager{
     int volume = 1;
 
     System.out.printf("Volume of the first bottle is: %d", volume);
-    
+
+
+    LiquorType();
+    //I don't remember Java that well. I am trying to remember what structures to use
+    //for each functionality. Do we use an array of strings for a list of liquor types or brands?
+    // I need to look at my old java programs, I remember using classes to manage other classes
     
     
     
---
+++ liquor.java
@@ -0,0 +1,11 @@
+public class liquor {
+    private short amount = -1;
+    
+    public void setAmount(int selectedAmount) {
+        this.amount = selectedAmount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
