d1ddbf82a47836ab99ffb83b359fb7bd2c9dc407
BSLang2006 <65793131+BSLang2006@users.noreply.github.com>
2023-01-31 11:48:22 -0500

    Add files via upload

---
+++ liquors.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Liquors {
+    private ArrayList<Liquor> liquorList = new ArrayList<Liquor>();
+
+
+    public void inputLiquor(Scanner scnr) {
+        String brand; //Jack Daniels
+        String type;  //Whiskey
+        short quantity; //accepts the amount .1 - 1
+    }
+
+
+
+}
