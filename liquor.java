b59537168c2f1d1f52777097a7da3909c3ce4ac9
Eric Hargrove <hargrove@mail.gvsu.edu>
2023-02-06 09:13:41 -0500

    Made testing branch, trying out the constructor

--- liquor.java
+++ liquor.java
@@ -13,4 +13,11 @@ public class liquor {
     String brand;
     double price;
     int volume;
+
+    public liquor(String t, String b, double p, int vol) {
+        type = t;
+        brand = b;
+        price = p;
+        volume = vol;
+    }
 }
