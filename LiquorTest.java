691bc7242ce39040747fe00e2038380c3c611768
Eric Hargrove <hargrove@mail.gvsu.edu>
2023-02-15 09:51:06 -0500

    JUnits!

--- LiquorTest.java
+++ LiquorTest.java
@@ -15,4 +15,17 @@ public class LiquorTest {
         test.setBrand("Brand");
         assertEquals("Brand", test.getBrand());
     }
+
+    @Test
+    public void testGetType() {
+        liquor test = new liquor("Gin", "Brand", 10.00, 40);
+        assertEquals("Gin", test.getType());
+    }
+
+    @Test
+    public void testSetType() {
+        liquor test = new liquor(null, null, 10.00, 40);
+        test.setType("Type");
+        assertEquals("Type", test.getType());
+    }
 }
