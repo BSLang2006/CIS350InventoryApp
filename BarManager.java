7459be85b455bd106469dc586c8baef286ce4bc4
Eric Hargrove <hargrove@mail.gvsu.edu>
2023-02-17 14:26:12 -0500

    Brainstorming adding new liquors to the inventory.

--- BarManager.java
+++ BarManager.java
@@ -3,6 +3,9 @@ import java.util.*;
 
 class BarManager{
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to Bar Inventory Manager"); 
     
         int volume = 1;
@@ -28,12 +31,48 @@ class BarManager{
 
         liquorList.add(testLiquor);
 
-        for (liquor liquor : liquorList) {
-            System.out.println(liquor.getBrand());
-        }
+//        for (liquor liquor : liquorList) {
+//            System.out.println(liquor.getBrand());
+//        }
 
         // Testing toString method
-        System.out.println(testLiquor);
-        System.out.println(Jack);
+//        System.out.println(testLiquor);
+//        System.out.println(Jack);
+
+        System.out.println("Current Inventory: ");
+        for (liquor liquor : liquorList) {
+            System.out.println(liquor);
+        }
+
+        String userInput;
+        System.out.println("Would you like to add to inventory? (y/n)");
+        userInput = scanner.next();
+
+        if (userInput.equals("y")) {
+            while (!userInput.equals("n")) {
+                liquor item = new liquor(null, null, 0, 0);
+                System.out.println("Enter Type: ");
+                String newType = scanner.next();
+                System.out.println("Enter Brand: ");
+                String newBrand = scanner.next();
+                System.out.println("Enter Price: ");
+                double newPrice = scanner.nextDouble();
+                System.out.println("Enter Volume: ");
+                int newVol = scanner.nextInt();
+                item.setVolume(newVol);
+                item.setPrice(newPrice);
+                item.setBrand(newBrand);
+                item.setType(newType);
+                liquorList.add(item);
+                System.out.println("Would you like to add more? (y/n)");
+                userInput = scanner.next();
+            }
+        }
+
+        for (liquor liquor : liquorList) {
+            System.out.println(liquor);
+        }
+
+
     }
 }
