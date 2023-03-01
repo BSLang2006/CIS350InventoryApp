7ed8a40857ad738d6c6ef1972fdf1ee776aa0aa3
Eric Hargrove <hargrove@mail.gvsu.edu>
2023-03-01 11:32:43 -0500

    Added classes for Beer and non-alcoholic drinks

---
+++ Beer.java
@@ -0,0 +1,5 @@
+public class Beer extends Liquor {
+    public Beer(String liqType, String liqBrand, double liqPrice, int liqVol) {
+        super(liqType, liqBrand, liqPrice, liqVol);
+    }
+}
---
+++ NonAlcohol.java
@@ -0,0 +1,6 @@
+public class NonAlcohol extends Liquor{
+
+    public NonAlcohol(String liqType, String liqBrand, double liqPrice, int liqVol) {
+        super(liqType, liqBrand, liqPrice, liqVol);
+    }
+}
