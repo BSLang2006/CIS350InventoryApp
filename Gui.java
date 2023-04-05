7991bde3b65d39f4f7585fcc31cda8b2ef1545ac
Eric Hargrove <hargrove@mail.gvsu.edu>
2023-04-05 09:48:32 -0400

    Cleaning up commented out code

--- Gui.java
+++ Gui.java
@@ -19,7 +19,6 @@ public class Gui {
         gui.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         panel.setLayout(new GridBagLayout());
-        //gbc.insets = new Insets(10,5,10,5);
 
         // for add button
         JLabel brandLabel = new JLabel("Brand");
@@ -88,14 +87,11 @@ public class Gui {
                 System.out.println(drinksList);
             }
         });
-        //JButton button = new JButton("Add");
         gbc.ipadx = 10;
         gbc.ipady = 2;
         gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.gridheight = 5;
-        //gbc.gridwidth = 5;
-        //gbc.fill = GridBagConstraints.VERTICAL;
         panel.add(addButton, gbc);
 
         //Select a row
@@ -116,12 +112,6 @@ public class Gui {
         gbc.gridx = 2;
         gbc.gridy = 0;
         panel.add(remButton, gbc);
-
-//                String[] sortOptions = {"brand", "type", "volume", "Price", "None"};
-//                JComboBox<String> sortList = new JComboBox<String>(sortOptions);
-//                gbc.gridx = 1;
-//                gbc.gridy = 1;
-//                panel.add(sortList, gbc);
 
         // Filter button logic
         filterButton.addActionListener(new ActionListener() {
@@ -144,112 +134,9 @@ public class Gui {
         gui.setSize(750, 600);
         gui.add(panel);
         gui.setVisible(true);
-
-
     }
 
     public static void main(String[] args) {
-//            Border border = new LineBorder(Color.BLACK, 4, true);
-//
-//            //creates the frame for the GUI
-//            JFrame gui = new JFrame("Bar Inventory");
-//            gui.setLayout(new GridLayout(1, 2));
-//            gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//            //Liquor Panel
-//            LiquorInventoryPanel liquorPanel = new LiquorInventoryPanel();
-//            liquorPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 0));
-//            liquorPanel.setBackground(Color.red);
-//            liquorPanel.setBorder(border);
-//
-//
-//            //Sets the title for the liquor panel
-//            JLabel liquorLabel = new JLabel("Liquor", SwingConstants.CENTER);
-//            liquorLabel.setFont(new java.awt.Font("Arial", Font.ITALIC, 25));
-//            liquorPanel.add(liquorLabel);
-//
-////        String[] optionsToChoose = {"Whiskey", "Brandy", "Vodka", "Rum", "None of the listed"};
-////        JComboBox<String> jComboBox = new JComboBox<>(optionsToChoose);
-////        panel.add(jComboBox);
-//
-//            //makes two textfields, one for the liquor information and the other
-//            //for whatever inputs you want to put in.
-//            JTextField textLiquor = new JTextField("The list of products will show here");
-//            JTextField textLiquor1 = new JTextField("Enter Product");
-//            //String inputText = textLiquor1.getText();
-//            textLiquor.setPreferredSize(new Dimension(300, 350));
-//            textLiquor.setBorder(border);
-//            textLiquor.setEditable(false);
-//
-//            textLiquor1.setPreferredSize(new Dimension(100, 30));
-//            textLiquor1.setBorder(border);
-//
-//            liquorPanel.add(textLiquor, textLiquor1);
-//            liquorPanel.add(new JButton("add"));
-//
-//            //attempting to add the data to the gui
-//            textLiquor.setToolTipText("Fill in the Blank");
-//            gui.add(liquorPanel);
-//
-//            //Beer panel
-//            BeerPanelClass beerPanel = new BeerPanelClass();
-//            beerPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 0));
-//            beerPanel.setBorder(border);
-//            beerPanel.setBackground(Color.YELLOW);
-//
-//            JLabel beerLabel = new JLabel("Beer", SwingConstants.CENTER);
-//            beerLabel.setFont(new java.awt.Font("Arial", Font.ITALIC, 25));
-//            beerPanel.add(beerLabel);
-//
-//            JTextField textBeer = new JTextField();
-//            JTextField textBeer1 = new JTextField();
-//
-//            textBeer.setPreferredSize(new Dimension(300, 350));
-//            textBeer.setBorder(border);
-//            textBeer.setEditable(false);
-//
-//            textBeer1.setPreferredSize(new Dimension(100, 30));
-//            textBeer1.setBorder(border);
-//
-//            beerPanel.add(textBeer, textBeer1);
-//            beerPanel.add(new JButton("add"));
-//            gui.add(beerPanel);
-//
-//            //NonAlcohol Panel
-//            NonAlcoholPanelClass nonAlcoholPanel = new NonAlcoholPanelClass();
-//            nonAlcoholPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 100, 0));
-//            nonAlcoholPanel.setBorder(border);
-//            nonAlcoholPanel.setBackground(Color.ORANGE);
-//
-//            JLabel nonAlcoholLabel = new JLabel("Non-Alcohol", SwingConstants.CENTER);
-//            nonAlcoholLabel.setFont(new java.awt.Font("Arial", Font.ITALIC, 25));
-//            nonAlcoholPanel.add(nonAlcoholLabel);
-//
-//            JTextField textNonAlcohol = new JTextField();
-//            JTextField textNonAlcohol1 = new JTextField();
-//
-//            textNonAlcohol.setPreferredSize(new Dimension(300, 350));
-//            textNonAlcohol.setBorder(border);
-//            textNonAlcohol.setEditable(false);
-//
-//            textNonAlcohol1.setPreferredSize(new Dimension(100, 30));
-//            textNonAlcohol1.setBorder(border);
-//
-//            nonAlcoholPanel.add(textNonAlcohol, textNonAlcohol1);
-//            nonAlcoholPanel.add(new JButton("add"));
-//            gui.add(nonAlcoholPanel);
-//
-//            //build the gui
-//
-//            gui.setSize(1250, 500);
-//            gui.setPreferredSize(new Dimension(1000, 500));
-//            gui.setVisible(true);
-//            gui.getContentPane().add(liquorPanel);
-//            gui.getContentPane().add(beerPanel);
-//            gui.getContentPane().add(nonAlcoholPanel);
-//
         new Gui();
-
     }
-
 }
